f2d0d8f48118c0af5ad29f5455f29c83aa7e8b0d
Jeremy Keene <Jeremy496@mac.com>
2025-09-24 00:18:03 +0000

    added button

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -4,24 +4,33 @@ import '../../styles/trafficLight.css'
 
 const TrafficLight = () => {
 	const [color,setColor] = useState('red');
+
+	const nextColor = () => {
+    setColor(prev => {
+      if (prev === 'red') return 'yellow';
+      if (prev === 'yellow') return 'green';
+      return 'red';
+    	});
+  	};
+
 	
 	return (
 		<>
             <div className="traffic-light-stem"></div>
             <div className="traffic-light-body">
 				<div 
-					className={color === "red" ? "light red selected" : "light red"}
-					onClick={() => setColor('red')}>
+					className={color === "red" ? "light red selected" : "light red"}>
 				</div>
 				<div 
-					className={color === "yellow" ? "light yellow selected" : "light yellow"}
-					onClick={() => setColor('yellow')}>
+					className={color === "yellow" ? "light yellow selected" : "light yellow"}>
 				</div>
 				<div 
-					className={color === "green" ? "light green selected" : "light green"}
-					onClick={() => setColor('green')}>
+					className={color === "green" ? "light green selected" : "light green"}>
 				</div>
             </div>
+			<div className="text-center mt-3">
+        		<button className="btn btn-success" onClick={nextColor}>Change Color</button>
+      		</div>
 	
 		
 	
